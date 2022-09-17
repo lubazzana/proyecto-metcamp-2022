6cfcdb9789acdae35a3aeee01c1b08b2199125bc
lubazzana <lucreciabazzana@gmail.com>
2022-09-17 16:44:25 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contact from './pages/contact';
 import Game from './pages/game';
 import Home from './pages/home';
+import packageInfo from '../package.json';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getBasename()}>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/game' element={<Game/>} />
@@ -17,6 +18,12 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+function getBasename() {
+  const array = packageInfo.homepage.split("/");
+
+  return array[array.length-1];
 }
 
 export default App;
